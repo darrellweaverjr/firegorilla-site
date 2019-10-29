188947a03a6eb05af9a3b7b57734891d23dcb20f
Darrell Weaver <darrellweaverjr@gmail.com>
2019-10-28 17:42:40 -0700

    added new graphics to agency solutions

--- resources/views/agency-solutions/agency-solutions.blade.php
+++ resources/views/agency-solutions/agency-solutions.blade.php
@@ -11,8 +11,48 @@
 
 <div class="solutions-body">
   <div class="container">
+
+    <div class="solutions-sect">
     <div class="row">
       <div class="col-md-6">
+        <div class="solutions-headline1">{{$agencySolutions->sectionOneTitle}}</div>
+        <div class="solutions-sub-txt">{{$agencySolutions->sectionOneSubtitle}}</div>
+        <div class="solutions-discover-txt">
+          {{$agencySolutions->sectionOneBody}}
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="solutions-pic">
+          <img src="https://storage.googleapis.com/fg-bucket/keywords.png" alt="Firegorilla">
+        </div>
+      </div>
+    </div>
+    </div>
+
+    <hr>
+
+    <div class="solutions-sect">
+    <div class="row">
+      <div class="col-md-7">
+        <div class="solutions-pic">
+          <img src="https://storage.googleapis.com/fg-bucket/ads.png" alt="Firegorilla">
+        </div>
+      </div>
+      <div class="col-md-5">
+        <div class="solutions-headline1">{{$agencySolutions->sectionThreeTitle}}</div>
+        <div class="solutions-sub-txt">{{$agencySolutions->sectionThreeSubtitle}}</div>
+        <div class="solutions-discover-txt">
+          {{$agencySolutions->sectionThreeBody}}
+        </div>
+      </div>
+    </div>
+    </div>
+
+    <hr>
+
+    <div class="solutions-sect">
+    <div class="row">
+      <div class="col-md-7">
         <div class="solutions-headline1">{{$agencySolutions->sectionTwoTitle}}</div>
         <div class="solutions-sub-txt">{{$agencySolutions->sectionTwoSubtitle}}</div>
         <div class="solutions-discover-txt">
@@ -21,11 +61,12 @@
       </div>
       <div class="col-md-6">
         <div class="solutions-pic">
-          <img src="https://storage.googleapis.com/fg-bucket/firegorilla-solution-1.png" alt="Firegorilla">
+          <!-- <img src="https://storage.googleapis.com/fg-bucket/firegorilla-solution-1.png" alt="Firegorilla"> -->
         </div>
       </div>
     </div>
-    <hr>
+    </div>
+
   </div>
 </div>
 @endforeach
